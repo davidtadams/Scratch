d86eb0f03f9bec52fd19f54da19a5d0489949a0d
davidtadams <adams.t.david@gmail.com>
2016-03-16 18:16:07 -0600

    adding basic gulp dev setup with bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,51 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var mainBowerFiles = require('main-bower-files')
+
+// var bowerJSPaths = [
+//   'bower_components/angular/angular.js',
+//   'bower_components/angular-ui-router/release/angular-ui-router.js',
+//   'bower_components/interact/dist/interact.js'
+// ]
+
+var sassPaths = [
+
+]
+
+/* ----------------------------------------------------------------------------
+  All Gulp development environment tasks
+---------------------------------------------------------------------------- */
+gulp.task('sass-dev', function() {
+  return gulp.src('frontend/content/scss/app.scss')
+    .pipe(sass({
+      includePaths: sassPaths
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('frontend/content/css'))
+})
+
+gulp.task('bower-dev', function() {
+  return gulp.src(mainBowerFiles())
+    .pipe(gulp.dest('frontend/scripts'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch(['frontend/content/scss/*.scss'], ['sass-dev'])
+})
+
+gulp.task('dev', ['sass-dev', 'bower-dev', 'watch'])
+
+/* ----------------------------------------------------------------------------
+  all gulp build tasks for production
+---------------------------------------------------------------------------- */
+gulp.task('sass-build', function() {
+  return gulp.src('frontend/content/scss/app.scss')
+    .pipe(sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('frontend/build/content/css'))
+})
+
+gulp.task('build', ['sass-build'])
